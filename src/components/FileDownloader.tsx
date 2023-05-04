7f72d8c59c4fe7967cbc9bc2a0dd2ffe5c0c9d7d
Георгий Пигарь <georgy-p@MacBook-Pro-Georgij.local>
2023-05-04 18:32:52 +0300

    add forms and handle function

--- src/components/FileDownloader.tsx
+++ src/components/FileDownloader.tsx
@@ -1,5 +1,22 @@
-const FileDownloader = () => {
-  return <div>FileDownload</div>;
+import { Col, Form } from "react-bootstrap";
+
+interface DownloadProps {
+  handleDownload: () => void;
+}
+
+const FileDownloader = ({ handleDownload }: DownloadProps) => {
+  const handleChange = () => {
+    handleDownload();
+  };
+
+  return (
+    <Col md={6} lg={6}>
+      <Form.Group className="mb-3 mt-3">
+        <Form.Label>Исходный документ</Form.Label>
+        <Form.Control type="file" onChange={handleChange} />
+      </Form.Group>
+    </Col>
+  );
 };
 
 export default FileDownloader;
